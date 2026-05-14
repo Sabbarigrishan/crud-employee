962b2d01050e2e4fad6f46058bc0f5264b354ae6
Sabbarigrishan <sabbari26504@gmail.com>
2026-05-14 16:11:04 +0530

    Connected frontend to backend

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8070/api/employees";
+const EMPLOYEE_API_BASE_URL ="https://crud-employee-backend-1-xy6p.onrender.com/api/employees";
 
 class EmployeeService{
 
